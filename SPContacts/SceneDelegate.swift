821da0223f7c1e25961ff62fcc6b50731d7e20c9
Sergey Parfenchyk <sergey.parfenchyk@gmail.com>
2022-04-09 15:46:42 +0300

    Initial project setup

---
+++ SPContacts/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  SPContacts
+//
+//  Created by Sergey Parfentchik on 9.04.22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow.init(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ SPContacts/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SPContacts
+//
+//  Created by Sergey Parfentchik on 9.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
