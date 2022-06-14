1a6af62b402067fbc044ebc51d35ea8a504f81c2
Sergey Parfenchyk <sergey.parfenchyk@gmail.com>
2022-06-14 16:00:38 +0300

    CREATED custom ErrorAccessContactsAlertController

---
+++ SPContacts/Modules/ErrorAccessContacts/ErrorAccessContactsAlertController.swift
@@ -0,0 +1,29 @@
+//
+//  ErrorAccessContactsAlertController.swift
+//  SPContacts
+//
+//  Created by Sergey Parfentchik on 14.06.22.
+//
+
+import UIKit
+
+class ErrorAccessContactsAlertController: UIAlertController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
